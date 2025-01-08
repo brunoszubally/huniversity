5a077393b85c69dcc031e25f0158dbc102de477e
Szubally Bruno <szubally.bruno@epistogram.com>
2025-01-08 16:06:08 +0100

    simple

--- app.py
+++ app.py
@@ -5,32 +5,18 @@ app = Flask(__name__)
 
 @app.route('/check', methods=['GET'])
 def check_student():
-    # SOAP kérés tartalma
     soap_request = """
     <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:okt="http://www.oktatas.hu/" xmlns:okt1="http://www.oktatas.hu">
       <soapenv:Header/>
       <soapenv:Body>
         <okt:Keres>
           <okt1:ApiKulcs>Hv-Tst-t312-r34q-v921-5318c</okt1:ApiKulcs>
-          <okt1:Azonosito>1223433576</okt1:Azonosito>
-          <okt1:IntezmenyRovidNev>KOSSUTH LAJOS ÁLTALÁNOS ISKOLA</okt1:IntezmenyRovidNev>
-          <okt1:IntezmenyTelepules>GYÖNGYÖSPATA</okt1:IntezmenyTelepules>
-          <okt1:JogosultNev>
-            <okt1:Elonev/>
-            <okt1:Keresztnev>Ádám</okt1:Keresztnev>
-            <okt1:Vezeteknev>Misuta</okt1:Vezeteknev>
-          </okt1:JogosultNev>
-          <okt1:LakohelyTelepules>GYÖNGYÖS</okt1:LakohelyTelepules>
-          <okt1:Munkarend>Nappali</okt1:Munkarend>
-          <okt1:Neme>Ferfi</okt1:Neme>
           <okt1:Oktazon>76221103192</okt1:Oktazon>
-          <okt1:SzuletesiEv>2010</okt1:SzuletesiEv>
         </okt:Keres>
       </soapenv:Body>
     </soapenv:Envelope>
     """
 
-    # SOAP kérés küldése
     url = "https://ws.oh.gov.hu/oktig-kartyaelfogado-test/publicservices.svc"
     headers = {
         "Content-Type": "text/xml; charset=utf-8",
